5933b4ca1db1b22ee6c7371a775e0269b958c31e
volker Struck <Struckvolker@web.de>
2024-07-16 07:36:23 +0200

    sec commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+console.log("V" + "O" + "L" + "K" + "E" + "R");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesnt work");
+
+// should return true. Change only the operator to fix it.
+console.log("50 + 2 += 52");
+
+// should be false. Change only the operator to fix it.
+console.log("333 == 333");
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log("600 + 510 / 2");
+
+// Wrong result: Expected a Number 4 received 22
+console.log("2 + 2");
+
+console.log("25 / 5"); // Result: 5
+console.log("50 / 10"); // Result: 5
